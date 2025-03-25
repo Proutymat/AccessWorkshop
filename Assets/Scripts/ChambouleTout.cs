ef5bc1c451975e903dc8a9d913f699850b98b177
Matéo Avventuriero <mateo.avventuriero@gmail.com>
2025-03-25 11:10:02 +0100

    feat: add chambouletout

---
+++ Assets/Scripts/ChambouleTout.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class ChambouleTout : MonoBehaviour
+{
+    
+    [SerializeField] private GameObject throwable;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
